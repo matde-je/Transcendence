e05de546c2b11647acb4068cfe136640e84faa8e
matde-je <matildecosta719@gmail.com>
2024-10-02 16:30:17 +0100

    things to do

--- frontend/static/spa_nav.js
+++ frontend/static/spa_nav.js
@@ -1,5 +1,20 @@
-const router = async () => {
-    const routes = [
-        {path: "/", view: () }
-    ]
-}
+// const router = async () => {
+//     const routes = [
+//         {path: "/"},
+//         {path: "/tournament"},
+//     ];
+
+//     const potentialMatch = routes.map(route => {
+//         return {
+//             route: route, 
+//             isMatch: location.pathname == route.path 
+//         };
+//     });
+
+//     let match = potentialMatch.find(potentialMatc => potentialMatc.isMatch );
+
+// };
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     router();
+// });
--- pong/views.py
+++ pong/views.py
@@ -4,5 +4,5 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'index.html') 
 
-# def tournament(request):
-#     return render(request, 'tournament.html')
+def tournament(request):
+    return render(request, 'tournament.html')
