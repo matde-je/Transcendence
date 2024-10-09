7b9aabbe24c0e0c5ea637b704faa0b24ae8a834d
matde-je <matildecosta719@gmail.com>
2024-10-09 17:47:13 +0100

    ssl https

--- transcendence/settings.py
+++ transcendence/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
     'pong',
 ]
 
@@ -111,7 +112,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
