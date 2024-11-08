c309786453de281bb8a41d3dde9689581536df67
Paulo <paulo.f.viegas@gmail.Home>
2024-11-08 16:32:33 +0000

    Admin Friendship updated

--- srcs/users/admin.py
+++ srcs/users/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
-from django.contrib import admin
 from .models import UserProfile, Friendship, MatchHistory
 
+class FriendshipAdmin(admin.ModelAdmin):
+    list_display = ('from_user', 'to_user', 'created_at', 'accepted_at', 'requested_by', 'accepted_by', 'accepted')
+    list_filter = ('accepted', 'created_at', 'accepted_at')
+    search_fields = ('from_user__username', 'to_user__username', 'requested_by__username', 'accepted_by__username')
+
 admin.site.register(UserProfile)
-admin.site.register(Friendship)
+admin.site.register(Friendship, FriendshipAdmin)
 admin.site.register(MatchHistory)
